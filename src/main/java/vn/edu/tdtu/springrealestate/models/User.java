428a9ec9ba0cab0c55d1c57a42162bc08344c9ce
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-23 14:49:44 +0700

    Add User.java

---
+++ src/main/java/vn/edu/tdtu/springrealestate/models/User.java
@@ -0,0 +1,28 @@
+package vn.edu.tdtu.springrealestate.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    private String email;
+    @Column
+    private String password;
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
