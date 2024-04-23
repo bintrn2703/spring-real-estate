bc8333aa052e2fab3843623c53d4ea7f40512f78
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-23 14:05:26 +0700

    Add login.html

--- src/main/java/vn/edu/tdtu/springrealestate/controllers/MainController.java
+++ src/main/java/vn/edu/tdtu/springrealestate/controllers/MainController.java
@@ -2,10 +2,16 @@ package vn.edu.tdtu.springrealestate.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class HomeController {
+public class MainController {
     @GetMapping("/")
     public String index() {
 
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
     }
 }
