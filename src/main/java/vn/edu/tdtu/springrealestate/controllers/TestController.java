bddd4dff9282b43e027d78d07e63a5354d25ea16
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-25 23:18:26 +0700

    Fix PropertyService

--- src/main/java/vn/edu/tdtu/springrealestate/controllers/TestController.java
+++ src/main/java/vn/edu/tdtu/springrealestate/controllers/TestController.java
@@ -2,6 +2,7 @@ package vn.edu.tdtu.springrealestate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,4 +30,5 @@ public class TestController {
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Hello to Test!!");
     }
+
 }
