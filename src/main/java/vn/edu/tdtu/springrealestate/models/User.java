d8df5bced09634cd6b2aa085ef5f9063b8f5dce8
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-29 19:12:23 +0700

    Update property-detail.html

--- src/main/java/vn/edu/tdtu/springrealestate/models/User.java
+++ src/main/java/vn/edu/tdtu/springrealestate/models/User.java
@@ -23,6 +23,8 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private String username;
+    @Column(name = "name")
+    private String name;
     @Enumerated(EnumType.STRING)
     private Role role;
 
